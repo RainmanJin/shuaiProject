e2229e2f965be67b9782f8c9ecf0254d41b8bbfa
wangshuai <399397446@qq.com>
2015-09-11 02:42:26 +0800

    20150911

---
+++ newMoer/js/controller.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wangshuai on 2015/9/6.
+ */
+
+angular.module("Myapp",[]).controller("MyCtrl",function($scope,$http){
+  $scope.msg = "hello";
+  $scope.user = [];
+  $http.get("./js/mytest.json").success(function(data){
+    console.log("123");
+    data = eval(data);
+    //$scope.user = data.name;
+    for(var i=0;i<data.length;i++){
+      $scope.user[i] = data[i].name;
+    }
+  }).error(function(){
+    console.warn("error");
+    $scope.user = "error";
+  })
+});
