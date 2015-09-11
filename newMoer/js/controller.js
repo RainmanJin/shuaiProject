550d3796f1ce091659183291bf019d09769b0a69
wangshuai <399397446@qq.com>
2015-09-11 17:50:48 +0800

    20150911office

--- newMoer/js/controller.js
+++ newMoer/js/controller.js
@@ -2,18 +2,41 @@
  * Created by wangshuai on 2015/9/6.
  */
 
-angular.module("Myapp",[]).controller("MyCtrl",function($scope,$http){
-  $scope.msg = "hello";
-  $scope.user = [];
-  $http.get("./js/mytest.json").success(function(data){
-    console.log("123");
-    data = eval(data);
-    //$scope.user = data.name;
-    for(var i=0;i<data.length;i++){
-      $scope.user[i] = data[i].name;
+var app = angular.module("moerApp",[]);
+//自选股大盘指数 JSON
+app.controller("stockIndex",function($scope,$http){
+  $scope.stockdp = [];
+  $http.get("./json/wapcommon_findStockIndexJson.json").success(function(data){
+    if(data.success == true){
+      data = eval(data);
+      var data1 = data.data;
+      var n = 0;
+      for(var i in data1){
+        $scope.stockdp[n] = data1[i];
+        n++;
+      }
     }
   }).error(function(){
     console.warn("error");
-    $scope.user = "error";
+    $scope.stockdp = "error";
+  })
+});
+
+//文章收益排行榜 JSON
+app.controller("articleRanking",function($scope,$http){
+  $scope.hotArticle = [];
+  $http.get("./json/frontindex_articleRankingJson.json").success(function(data){
+    if(data.success == true){
+      data = eval(data);
+      var data1 = data.data;
+      var n = 0;
+      for(var i in data1){
+        $scope.hotArticle[n] = data1[i];
+        n++;
+      }
+    }
+  }).error(function(){
+    console.warn("error");
+    $scope.stockdp = "error";
   })
 });
